550b526d2d1587c6a92fb2a6c079085c09608b06
ritesh <ritesh.sonawala@gmail.com>
2019-04-14 08:18:49 +0100

    Initial Add

---
+++ src/main/java/com/rs/BeamPocApplication.java
@@ -0,0 +1,13 @@
+package com.rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeamPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeamPocApplication.class, args);
+	}
+
+}
